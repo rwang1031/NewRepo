06c4e6ad0c4f8ac36f2c1da8457710f7f7017925
rwang1031@gmail.com <rwang1031@gmail.com>
2020-11-18 02:31:57 -0500

    fdg

--- api.js
+++ api.js
@@ -8,14 +8,14 @@ var http = require('http');
 const { request } = require('express');
 //var cors = require('cors');
 var app = express();
-//var router = express.Router();
+var router = express.Router();
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-//app.use('/api',router);
+app.use('/api',router);
 
 
-/* router.use((request,response,next)=>{
+router.use((request,response,next)=>{
     console.log('middleware')
     next();
 })
@@ -26,39 +26,6 @@ router.route('/orders').get((request,response)=>{
     }) 
 
 })
- */
-const { auth } = require('express-openid-connect');
-
-const config = {
-  authRequired: true,
-  auth0Logout: true,
-  secret: process.env.Auth0Secret,
-  baseURL: 'https://api11.azurewebsites.net/',
-  clientID: 'bAzoU1hYJfvpPJ7N5deAFKFzb0A7BwYO',
-  issuerBaseURL: 'https://dev-ub3msj1r.us.auth0.com'
-};
-
-
-const { requiresAuth } = require('express-openid-connect');
-
-
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
-
-// req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-});
-
-app.get('/profile', requiresAuth(), (req, res) => {
-    res.send(JSON.stringify(req.oidc.user));
-});
-
-app.get('/api/orders', requiresAuth(), (req, res) => {
-    dboperations.getOrders().then(result=>{
-        res.json(result.recordset);    
-    }) 
-});
 
 var port = process.env.port || 443;
 
