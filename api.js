b3655ef5004119aced4a1ee93a4dd0e60973483e
rwang1031@gmail.com <rwang1031@gmail.com>
2020-11-18 21:45:30 -0500

    ddd

--- api.js
+++ api.js
@@ -4,6 +4,8 @@ var Order = require('./order');
 
 var express = require('express');
 var bodyParser = require('body-parser');
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
 var http = require('http');
 const { request } = require('express');
 //var cors = require('cors');
@@ -26,6 +28,24 @@ router.route('/orders').get((request,response)=>{
     }) 
 
 })
+
+var jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://dev-ub3msj1r.us.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'https://api11.azurewebsites.net/api/orders',
+  issuer: 'https://dev-ub3msj1r.us.auth0.com/',
+  algorithms: ['RS256']
+});
+
+app.use(jwtCheck);
+
+app.get('/authorized', function (req, res) {
+    res.send('Secured Resource');
+});
 
 var port = process.env.port || 443;
 
